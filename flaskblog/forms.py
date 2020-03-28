26a87889f9a2a6cf300b92f5f6b51c621658daac
Jeremy Libre <jw.libre@hotmail.com>
2020-03-28 17:14:41 +0000

    End of tutorial 7 - profile updating and picture resizing

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+from flask_login import current_user
 
 # FlaskForm is a class, RegistrationForm (and all other types of form we create)
 # are classes that will inherit from this class.
@@ -19,17 +21,38 @@ class RegistrationForm(FlaskForm):
     # example taken from wtforms documentation
 
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first().username
+        user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError(f'Username "{user}" already exists, please choose another.')
+            raise ValidationError(f'Username "{username.data}" already exists, please choose another.')
 
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first().email
+        user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError(f'Email "{user}" already exists, please choose another.')
+            raise ValidationError(f'Email "{email.data}" already exists, please choose another.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Log In')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+
+    # create a custom validation within the form to confirm that the username/email doesn't already exist
+    # example taken from wtforms documentation
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(f'Username "{username.data}" already exists, please choose another.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(f'Email "{email.data}" already exists, please choose another.')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,11 @@
 from flask import render_template, url_for, flash, redirect, request
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flaskblog import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+import os
+from PIL import Image
 
 # flaskblog is the package name.
 # importing from flaskblog == importing from __init__.py
@@ -75,8 +78,37 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    # rename the file to a random hex string using secrets
+    random_hex = secrets.token_hex(8) # 8 bytes long
+    # extract extension
+    _, f_ext = os.path.splitext(form_picture.filename) # underscore convention here is to represent variable that is not used in subsequent code
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    # scale down image to save space on filesystem, and to make website faster
+    output_size = (100, 100)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
 
-@app.route('/account')
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            # set the user's profile picture, and add it to the database
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated.', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename = 'profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
